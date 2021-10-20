9a2b5d5cc30d5211c92bded934da00f30ebbeba4
BernatOlle <90847284+BernatOlle@users.noreply.github.com>
2021-10-21 01:09:44 +0200

    Update channel.cpp

--- src/channel.cpp
+++ src/channel.cpp
@@ -1,1 +1,20 @@
+#include "channel.hpp"
 
+
+channel::channel(int xid){
+    id=xid;
+    num = Global_params::Global_params()->get_nchannels();
+}
+
+int Channel:: get_channel_Id(){
+ return id;
+
+}
+
+void Channel:: set_channel_id(int nid){
+this->id=nid;
+}
+
+int Channel:: get_nchannels(){
+     return num;
+}
