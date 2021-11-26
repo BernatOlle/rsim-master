e820f58899ce64eeed8888b22ee7d8a49820dacc
BernatOlle <bernatollef@gmail.com>
2021-11-26 19:05:41 +0100

    Solving errors

--- src/global_params.hpp
+++ src/global_params.hpp
@@ -119,13 +119,13 @@ public:
     int get_ids_concurrent_tx_nodes_size(); // Get size of ids_concurrent_tx_nodes (number of nodes simultaneously transmitting)
     int get_ids_and_channels_concurrent_tx_nodes_size(); // Get size of ids_and_channels_concurrent_tx_nodes (number of nodes simultaneously transmitting)
     int get_channel_concurrent_tx_nodes_size(); // Get size of channel_concurrent_tx_nodes (number of channels concerned by the simultaneous transmition)
-    void push_ids_concurrent_tx_nodes(int nid); // Push new node_id into ids_concurrent_tx_nodes (new node starts transmitting)
+    void push_ids_concurrent_tx_nodes(std::vector<int> nid); // Push new node_id into ids_concurrent_tx_nodes (new node starts transmitting)
     void push_ids_and_channels_concurrent_tx_nodes(std::vector<int> ncid); // Push new node_id and channel_id into ids_and_channels_concurrent_tx_nodes (new node starts transmitting)
     void push_channel_concurrent_tx_nodes(int cid); // Push channel_id into ids_concurrent_tx_nodes
     void flush_ids_concurrent_tx_nodes(); // Delete all elements in ids_concurrent_tx_nodes (no node will be transmitting)
     void flush_ids_and_channels_concurrent_tx_nodes(); // Delete all elements in ids_and_channels_concurrent_tx_nodes (no node will be transmitting)
     void flush_channel_concurrent_tx_nodes(); // Delete all elements in channel_concurrent_tx_nodes (no channel will be used for transmission)
-    void delete_ids_concurrent_tx_nodes(int cid, std::vector<int> nid); // Delete all nodes that finished transmitting for the given channel
+    void delete_ids_concurrent_tx_nodes(int cid, std::vector<int> *curr_node); // Delete all nodes that finished transmitting for the given channel
     int get_unique_ids_concurrent_tx_nodes(); // If we have ensured that there's only 1 tx node, we return its id
     std::vector<int>::const_iterator ids_concurrent_tx_nodes_begin();
     std::vector<int>::const_iterator ids_concurrent_tx_nodes_end();
